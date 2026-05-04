70adf8c4ff80e3b8da1253b6d9869a2d35230a72
Gonçalo Ribeiro <darklaw13@yahoo.co.uk>
2026-05-04 08:21:35 +0100

    Convert from .html, not .txt
    With the usage of <pre> this results in much much simpler HTML in the
    EPUB. This hopefully makes them faster to load on e-readers.

--- convert.sh
+++ convert.sh
@@ -1,12 +1,31 @@
 #!/usr/bin/bash
 
 CSS="$(dirname $0)/phrack.css"
+TMPFILE=$(mktemp /tmp/phrack.XXXXXX.html)
 
 for txt in $(ls phrack*.txt); do
     base=${txt%.*};
     title=$(echo $base | sed -e 's/[0-9]/ \0/')
     title=${title^}  # uppercase first character
 
-    ebook-convert "$txt" "$base.epub" --paragraph-type=off --preserve-spaces --extra-css="$CSS" --formatting-type=plain --paragraph-type=single --remove-paragraph-spacing --remove-paragraph-spacing-indent-size=-1 --margin-left=0 --margin-right=0 --no-default-epub-cover --embed-all-fonts --disable-font-rescaling --title="$title"
+    cat > "$TMPFILE" <<EOF
+<html>
+<head>
+    <title>$title</title>
+</head>
+<body>
+<pre>
+EOF
+
+    cat "$txt" >> "$TMPFILE"
+
+    cat >> "$TMPFILE" <<EOF
+</pre>
+</body>
+EOF
+
+    ebook-convert "$TMPFILE" "$base.epub" --extra-css="$CSS" --margin-left=0 --margin-right=0 --no-default-epub-cover --embed-all-fonts --disable-font-rescaling --title="$title"
 
 done
+
+rm "$TMPFILE"
